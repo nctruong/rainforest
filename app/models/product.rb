023cc944f7978e6639deab168fad75ff8f651428
Joseph Kim <jkim0120@hotmail.com>
2015-06-30 15:00:39 -0400

    Added associated Review model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :reviews
+  has_many :users, through: :reviews
+
   validates :description, :name, presence: true
   validates :price_in_cents, format: { with: /\A[+]?(\d+|\d+\.\d{2})\Z/, message: "is in the wrong format [ $ 0.00 ]" }
 
