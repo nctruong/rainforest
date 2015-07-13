c00768fc5a9db48d9d0f1814eede5cac4aee2129
Joseph Kim <jkim0120@hotmail.com>
2015-07-13 02:19:39 -0400

    Troublshooting

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -25,7 +25,7 @@ class ButtonsController < ApplicationController
   end
 
   def create
-    @button = current_user.buttons.build(core_id: params[:core_id], access_token: @@access_token)
+    @button = current_user.buttons.new(core_id: params[:core_id], access_token: @@access_token)
     @button.user = current_user
 
     if @button.save
